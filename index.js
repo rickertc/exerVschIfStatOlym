0f78a26df626924cee8629965a9c8edc71450035
rickertc <u1366385@umail.utah.edu>
2021-11-21 11:59:35 -0800

    Bobby

---
+++ index.js
@@ -0,0 +1,52 @@
+if(true){
+    console.log("It is true!")
+}
+
+if(5 > 3){
+    console.log("is greater than")
+}
+
+var cat = 3
+if(cat === 3){
+    console.log("is the length")
+} 
+var cat = cat 
+var dog = dog
+if(cat === dog){
+    console.log("it is equal")
+} else {
+    console.log("not the same")
+}
+
+  var person = {
+    name: "Bobby",
+    age: 12 
+  }
+
+  if(person.age < 18){
+    console.log("Bobby is not allowed to go to the movie")
+  }
+if(person.age > 18){
+    console.log("Bobby is allowed to go to the moveie")
+}
+
+if(person.name[0] === "B"){
+    console.log("Bobby is allowed to go to the movie")
+}
+if(person.name[0] === "B" && person.age > 18){
+    console.log("Bobby is allowed to go to the movie")
+}
+
+if(1 === 1){
+      console.log("strict")
+  } else if(1 == "1"){
+      console.log("loose")
+  } else {
+      console.log("not equal at all")
+  }
+
+  if(1 <= 2 && 2 === 4){
+      console.log("yes")
+
+  }
+  
